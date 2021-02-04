71a71eaf6dbba52b01c8a03a20018a6cc56ccc22
sachink4s <67131301+sachink4s@users.noreply.github.com>
2021-02-04 17:42:30 +0000

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -30,7 +30,7 @@ def send_start(bot, update):
         chat_id=update.chat.id,
         text=script.START_TEXT.format(update.from_user.first_name),
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="✅ Channel ✅", url="https://t.me/SK4S_AllChannel")], [InlineKeyboardButton(text="✅ Support ✅", url="https://t.me/SK4S_BotSupport"),
-                                                    InlineKeyboardButton(text="✅ Share ✅", url="tg://msg?text=%2A%2AHai%20%E2%9D%A4%EF%B8%8F%2C%2A%2A%20%0A__Today%20i%20just%20found%20out%20an%20intresting%20and%20Powerful__%20%2A%2AZee5%20Downloader%20Bot%2A%2A%20__for%20Free%F0%9F%A5%B0.__%20%20%0A%2A%2ABot%20Link%20%3A%20%40Zee5HEXBot%2A%2A%20%F0%9F%94%A5")]]),
+                                                    InlineKeyboardButton(text="✅ Share ✅", url="tg://msg?text=%2A%2AHai%20%E2%9D%A4%EF%B8%8F%2C%2A%2A%20%0A__Today%20i%20just%20found%20out%20an%20intresting%20and%20Powerful__%20%2A%2AZee5%20Downloader%20Bot%2A%2A%20__for%20Free%F0%9F%A5%B0.__%20%20%0A%2A%2ABot%20Link%20%3A%20%40SK4S_Zee5DLBot%2A%2A%20%F0%9F%94%A5")]]),
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
