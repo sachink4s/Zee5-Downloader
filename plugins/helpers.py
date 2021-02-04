314813c122cb5f4cc187ee2bb268f372b5552d39
sachink4s <67131301+sachink4s@users.noreply.github.com>
2021-02-04 16:31:37 +0000

    Update helpers.py

--- plugins/helpers.py
+++ plugins/helpers.py
@@ -47,12 +47,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "\n[{0}{1}] \n\n⭕️Progress: {2}%\n".format(
-            ''.join(["▣" for i in range(math.floor(percentage / 5))]),
-            ''.join(["▢" for i in range(20 - math.floor(percentage / 5))]),
+        progress = "\n[{0}{1}] \n\n● Progress: {2}%\n".format(
+            ''.join(["🔵" for i in range(math.floor(percentage / 5))]),
+            ''.join(["🔴" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\n\n⭕️Speed: {2}/s\n\n⭕️ETA: {3}\n".format(
+        tmp = progress + "{0} of {1}\n\n● Speed: {2}/s\n\n● ETA: {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
